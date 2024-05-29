b42ac469fda931023e2826f527cc6642fd3d0b7b
asadullah48 <asadullahshafique@hotmail.com>
2024-05-29 12:04:24 +0500

    Initial commit

---
+++ ans124.js
@@ -0,0 +1,11 @@
+"use strict";
+// Q 124
+// Defines an object with a name property and a method to return its name
+const person = {
+    name: "Asad",
+    getName: function () {
+        return this.name; // Uses 'this' to refer to the object itself and return its 'name' property
+    },
+};
+console.log(person.getName()); // Outputs: Alice
+// This method correctly identifies and returns the object's 'name' property using 'this'.
